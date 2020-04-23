01d0b392efde1069b6a6c58a453187ba94212fd8
tima <tima.ogurtzova@yandex.ru>
2020-04-23 16:57:42 +0300

    adminpage/userpage

--- src/main/java/com/hellocat/springBootTest/controller/AdminController.java
+++ src/main/java/com/hellocat/springBootTest/controller/AdminController.java
@@ -5,6 +5,8 @@ import com.hellocat.springBootTest.domen.User;
 import com.hellocat.springBootTest.service.RoleService;
 import com.hellocat.springBootTest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -25,24 +27,17 @@ public class AdminController {
     public String showAdminPage(Model model) {
         List<User> users = userService.findAllUsers();
         List<Role> roles = roleService.findAllRoles();
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByName(auth.getName());
+        if (user == null) {
+            return "redirect:/logout";
+        }
         model.addAttribute("users", users)
                 .addAttribute("roles", roles)
-                .addAttribute("userNew", new User());
+                .addAttribute("userNew", new User())
+                .addAttribute("userAuth", user);
         return "admin/adminPage";
-    }
-
-    @GetMapping("/update/{id}")
-    public String showUpdatePage(@PathVariable String id, Model model) {
-        try {
-            Long idLong = Long.parseLong(id);
-            User user = userService.findUserById(idLong);
-            model.addAttribute("user", user)
-                    .addAttribute("roles", roleService.findAllRoles());
-            return "admin/updatePage";
-        } catch (NumberFormatException e) {
-            //
-        }
-        return "redirect:";
     }
 
     @PostMapping("/update")
--- src/main/java/com/hellocat/springBootTest/controller/UserController.java
+++ src/main/java/com/hellocat/springBootTest/controller/UserController.java
@@ -1,6 +1,12 @@
 package com.hellocat.springBootTest.controller;
 
+import com.hellocat.springBootTest.domen.User;
+import com.hellocat.springBootTest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public String userPage() {
+    public String userPage(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByName(auth.getName());
+        model.addAttribute("userAuth", user);
         return "user/userpage";
     }
 
